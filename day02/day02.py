db23dcd17ebccb4fb163e9ffea179ffca105efd4
TeeJizzm <teejizzm@gmail.com>
2025-12-02 22:06:16 +0000

    Prep Day 02

--- day02/day02.py
+++ day02/day02.py
@@ -16,42 +16,44 @@ import tools.texttolists as tl
 # Variables
 
 
-
 ############################
 # Functions
 
+
 def day02(text):
-    print("Day 02 - *NAME*")
-    
-    part1, part2 = text, ''
-    
-    
-    
+    print("Day 02 - Gift Shop")
+
+    part1, part2 = text, ""
+
+    ranges = tl.to2dLists(text, ",", "-")
+
+    print(ranges)
+
     return part1, part2
+
 
 ############################
 # Run main
 
 if __name__ == "__main__":
-    day = "day02/inc" 
-    
+    day = "day02/inc"
+
     # Change file
     #######
     file = "ex.txt"
-    #file = "in.txt"
+    # file = "in.txt"
     #######
-    
+
     # Get absolute filepath of file
     filepath = os.path.join(os.getcwd(), day, file)
-    
+
     # Open file, clean up memory after
     with open(filepath, "r") as file:
-        
-        text = file.read() # Read data
+        text = file.read()  # Read data
 
     part1, part2 = day02(text)
 
     print("Part 1: ", part1)
     print("Part 2: ", part2)
-    
+
 ########### EOF ############
